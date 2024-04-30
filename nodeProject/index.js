2bdc3dd06d5e42adabc5769795e529b2144a308d
Júnior Nunes <denisonprobable13@gmail.com>
2024-04-30 20:22:42 -0300

    feat: create node project

---
+++ nodeProject/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Full Cycle</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
