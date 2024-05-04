6a9a9bec378d70989e47a42ef428189f4af94803
Júnior Nunes <denisonprobable13@gmail.com>
2024-05-03 23:17:33 -0300

    fix: node working with nginx

--- nodeProject/index.js
+++ nodeProject/index.js
@@ -4,9 +4,14 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Full Cycle</h1>");
+    res.send(`
+        <div>
+        <h1>Full Cycle!</h1>
+        <h2>Desafio Docker</h2>
+        </div>
+    `);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server is running on port ${PORT}`);
 });
